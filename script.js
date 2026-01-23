ee5c08546f11bb9c26fafb7e7c8424e4fdb76820
Henry Chou <henrychou@Mac-mini.local>
2026-01-22 21:30:56 -0500

    Add $ to total in notification email, third try

--- script.js
+++ script.js
@@ -399,7 +399,7 @@ function showCheckoutForm() {
         <form class="checkout-form" name="order" method="POST" netlify onsubmit="handleCheckout(event)">
             <input type="hidden" name="form-name" value="order">
             <textarea name="cart-items" style="display:none">${cartSummary}</textarea>
-            <input type="hidden" name="order_total" value="$${total.toFixed(2)}">
+            <textarea name="total" style="display:none">$${total.toFixed(2)}</textarea>
             
             <h3 style="font-family: 'Alice', serif; color: #554319; margin-bottom: 0.5rem;">Checkout</h3>
             
